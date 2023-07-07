bf8ffaec1f4e680bd5be191669b21e49fa3504a3
Amanmandal-M <amanmandal644@gmail.com>
2023-07-08 01:46:20 +0530

    Added Updated Code

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -13,10 +13,21 @@ const loginBtn = document.querySelector("#btnLogin");
 
 // Input Fields
 const signname = document.querySelector(".signupName");
-const signemail = document.querySelector(".signupEmail");
-const signpassword = document.querySelector(".signupPass");
-const loginEmail = document.querySelector(".loginEmail");
-const loginPass = document.querySelector(".loginPass");
+const signEmail = document.querySelector(".signupEmail");
+const signPass = document.querySelector(".signupPass");
+const loginEmail = document.querySelector('.loginEmail');
+const loginPass = document.querySelector('.loginPass');
+
+// Spinner icon
+const showSpinner = () => {
+    signUpBtn.innerHTML="";
+    signUpBtn.innerHTML = `<div class="spinner"></div>`
+};
+  
+const hideSpinner = () => {
+    signUpBtn.innerHTML="";
+    signUpBtn.textContent="Sign Up";
+};
 
 signUpLink.addEventListener('click', () => {
     wrapper.classList.add('animate-signIn');
@@ -28,14 +39,118 @@ signInLink.addEventListener('click', () => {
     wrapper.classList.remove('animate-signIn');
 });
 
+// Empty all fields 
 
-// Register and login Buttons
-signUpBtn.addEventListener('click',(e)=>{
+function emptyAllFields() {
+    signname.value="";
+    signEmail.value="";
+    signPass.value="";
+    loginEmail.value="";
+    loginPass.value="";
+}
+
+// SignUp Button Event Listener
+signUpBtn.addEventListener('click', async (e) => {
     e.preventDefault();
-    
+    signupSection();
 });
 
-loginBtn.addEventListener('click',(e)=>{
-    e.preventDefault();
+const signupSection = async () =>{
+    showSpinner();
+    let obj = {
+        name: signname.value,
+        email: signEmail.value,
+        password: signPass.value
+    };
+
     
-})
+    try {
+        const fetchingUrl = await fetch(registerUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        });
+        const apiResponse = await fetchingUrl.json();
+
+        if(fetchingUrl.status==200){
+            hideSpinner();
+            Swal.fire({
+                icon: 'success',
+                text: 'Sign Up Successfully',
+                width: "22%",
+                didClose: () => {
+                  wrapper.classList.add('animate-signUp');
+                  wrapper.classList.remove('animate-signIn');
+                }
+              });
+              emptyAllFields()  
+        }else{
+            emptyAllFields()
+            Swal.fire({
+                icon: 'error',
+                text: 'Credentials Not Found',
+                width: "22%"
+            });
+        }
+    } catch (error) {
+        hideSpinner();
+        emptyAllFields();
+        Swal.fire({
+            icon: 'error',
+            text: 'Sign Up Failed',
+            width: "22%"
+        });
+    }
+}
+
+// Login Button Event Listener
+loginBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    loginSection();
+});
+
+const loginSection = async () => {
+    showSpinner();
+    let obj = {
+        email: loginEmail.value,
+        password: loginPass.value
+    };
+
+    try {
+        const fetchingUrl = await fetch(loginUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        });
+        const apiResponse = await fetchingUrl.json();
+        if(fetchingUrl.status === 200) {
+            hideSpinner();
+            Swal.fire({
+                icon: 'success',
+                text: 'Login Successful',
+                width: "22%"
+            });
+            emptyAllFields();
+        }else{
+            emptyAllFields()
+            Swal.fire({
+                icon: 'error',
+                text: 'Credentials Not Found',
+                width: "22%"
+            });
+        }
+    } catch (error) {
+        hideSpinner();
+        // Handle login error
+        emptyAllFields();
+        Swal.fire({
+            icon: 'error',
+            text: 'Login Failed',
+            width: "22%"
+        });
+    }
+};
