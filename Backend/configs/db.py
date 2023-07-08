80593c047264f7a274efdc532c7a6109221c24c9
Amanmandal-M <amanmandal644@gmail.com>
2023-07-09 02:39:40 +0530

    Configs DOne

--- Backend/configs/db.py
+++ Backend/configs/db.py
@@ -1,13 +1,14 @@
-import os
 from pymongo import MongoClient
 from dotenv import dotenv_values
 
 env_vars = dotenv_values('.env')
 
-Mongo_link = os.getenv('MONGO_URI')
+Mongo_link = env_vars.get('MONGO_URI')
 
 # Establish connection to MongoDB
 client = MongoClient(Mongo_link)
+db = client['ZomatoDB']
 
 # Export the database connection
 dbConnection = db
+
