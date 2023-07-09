4642d189cea3ad957b4808016212fda078907666
Amanmandal-M <amanmandal644@gmail.com>
2023-07-09 16:12:15 +0530

    Upadated code

--- Backend/configs/db.py
+++ Backend/configs/db.py
@@ -1,9 +1,7 @@
+import os
 from pymongo import MongoClient
-from dotenv import dotenv_values
 
-env_vars = dotenv_values('.env')
-
-Mongo_link = env_vars.get('MONGO_URI')
+Mongo_link = os.getenv('MONGO_URI')
 
 # Establish connection to MongoDB
 client = MongoClient(Mongo_link)
