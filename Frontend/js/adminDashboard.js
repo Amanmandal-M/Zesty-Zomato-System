e0836a0444b3b16d157a7220aa08d0697a42ff0a
Amanmandal-M <amanmandal644@gmail.com>
2023-08-01 23:11:20 +0530

    Adding Admin authentication

--- Frontend/js/adminDashboard.js
+++ Frontend/js/adminDashboard.js
@@ -5,3 +5,39 @@ function userLogin() {
 function home() {
   window.location.href = "../index.html";
 }
+
+const loginSection = document.querySelector('.loginSection');
+const bodyLoad = document.querySelector('body');
+
+
+const loadAdmin = async () => {
+    const { value: password } = await Swal.fire({
+      title: 'Enter Secret Key',
+      input: 'password',
+      inputLabel: 'Password',
+      inputPlaceholder: 'Enter Secret Key',
+      inputAttributes: {
+        maxlength: 9,
+        autocapitalize: 'off',
+        autocorrect: 'off'
+      },
+      didClose: () => {
+        validation(password)
+      }
+    })
+    
+
+
+    async function validation (password) {
+        try {
+          console.log(password)
+        } catch (error) {
+           swal.fire({
+            title: title,
+            text: message,
+            icon: icon,
+           })
+        }
+    }
+}
+
